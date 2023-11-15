2e252a634d075bfa93aed87ad1334f3e6e6c70e1
camved <cam.ved@outlook.fr>
2023-11-15 14:37:21 +0100

    On branch master Your branch is up to date with 'origin/master'.
    Changes to be committed:
    	modified:   Tokens.py
    
    Changes not staged for commit:
    	modified:   SyntaxAnalayser.py

---
+++ Tokens.py
@@ -0,0 +1,3 @@
+from enum import Enum
+
+TYPE = Enum('TYPE',['motcle', 'ident', 'ch', 'virg', 'ptvirg', 'point', 'deuxpts', 'parouv', 'parenf', 'inf', 'sup', 'eg', 'plus', 'moins', 'mult', 'divi', 'infe', 'supe', 'diff', 'aff' ])
